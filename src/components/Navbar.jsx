3aa75523e57b985292ece22768c6b171c90edb40
Sameer Khan A <sameerkhan003a@gmail.com>
2026-07-23 17:08:06 +0530

    Update Navbar links and structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,11 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg fixed-top shadow-sm py-2" id="custom-navbar">
       <div className="container-fluid px-3 px-lg-5">
-        {/* Logo + Brand */}
-        <a className="navbar-brand d-flex align-items-center gap-2 fw-bold" href="#">
+        <a className="navbar-brand d-flex align-items-center gap-2 fw-bold" href="/">
           <img src="/logo.png" alt="V TECH Logo" height="45" width="50" />
           <span className="brand-text" style={{ color: 'white' }}>V TECH SOLUTIONS</span>
         </a>
 
-        {/* Mobile Toggle */}
         <button
           className="navbar-toggler custom-toggler"
           type="button"
@@ -26,15 +24,19 @@ const Navbar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        {/* Nav Items */}
         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
           <ul className="navbar-nav align-items-lg-center w-100 justify-content-end">
-            
-            {/* Home Dropdown */}
+
             <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <button
+                className="nav-link dropdown-toggle btn btn-link border-0 text-decoration-none"
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                style={{ color: 'inherit' }}
+              >
                 Home
-              </a>
+              </button>
               <ul className="dropdown-menu">
                 <li><a className="dropdown-item" href="#home">Home</a></li>
                 <li><a className="dropdown-item" href="#MissionVision">About Us</a></li>
@@ -46,34 +48,36 @@ const Navbar = () => {
               </ul>
             </li>
 
-            <li className="nav-item">
-              <a className="nav-link" href="/Courses">Remote Learning</a>
-            </li>
+            <li className="nav-item"><a className="nav-link" href="/Courses">Remote Learning</a></li>
+            <li className="nav-item"><a className="nav-link" href="/CoursesOffered">Courses</a></li>
+            <li className="nav-item"><a className="nav-link" href="/VtechCACS">CA/CS Coaching</a></li>
 
-            <li className="nav-item">
-              <a className="nav-link" href="/CoursesOffered">Courses</a>
-            </li>
-
-            <li className="nav-item">
-              <a className="nav-link" href="/VTechCACS">CA/CS Coaching</a>
-            </li>
-
-            {/* Events */}
             <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <button
+                className="nav-link dropdown-toggle btn btn-link border-0 text-decoration-none"
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                style={{ color: 'inherit' }}
+              >
                 Events
-              </a>
+              </button>
               <ul className="dropdown-menu">
                 <li><a className="dropdown-item" href="/VTechActivities">Activities</a></li>
                 <li><a className="dropdown-item" href="/VTechGallery">Gallery</a></li>
               </ul>
             </li>
 
-            {/* Placements */}
             <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <button
+                className="nav-link dropdown-toggle btn btn-link border-0 text-decoration-none"
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                style={{ color: 'inherit' }}
+              >
                 Placements
-              </a>
+              </button>
               <ul className="dropdown-menu">
                 <li><a className="dropdown-item" href="/HiringDetails">Hiring Partners</a></li>
                 <li><a className="dropdown-item" href="/SuccessDetails">Success Stories</a></li>
